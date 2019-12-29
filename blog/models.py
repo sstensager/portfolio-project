4de75c9262f9560c4a9cf707cf4897b5edb5e9df
Steven Stensager <stevenstensager@MacBook-Pro.attlocal.net>
2019-12-28 21:27:08 -0800

    add blog model and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	image = models.ImageField(upload_to='images/')
+	title = models.CharField(max_length=200)
+	pub_date = models.DateField()
+	body = models.CharField(max_length=1000)
+
+
+
+# title
+#  pub_date
+#  body
+#  image
+
+# Create a blog models
+
+# Add the blog app to the settings
+
+# Create a migration
+
+# migrate
+
+# add to the admin
